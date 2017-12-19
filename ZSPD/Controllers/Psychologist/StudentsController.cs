f448b7f6ce15d666a9a9a108a7ca7888df80049a
Stefan Pająk <stefpaj@o2.pl>
2017-12-19 21:17:15 +0100

    Zapis wyników ankiet do excela

--- ZSPD/Controllers/Psychologist/StudentsController.cs
+++ ZSPD/Controllers/Psychologist/StudentsController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using ClosedXML.Excel;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -78,6 +80,69 @@ namespace ZSPD.Controllers.Psychologist
             var user = students.FirstOrDefault(x => x.Id == userId);
 
             return View(user);
+        }
+
+        public ActionResult ExcelResults(string userId)
+        {
+            var students = _psychologistManager.GetAllStudents();
+            var user = students.FirstOrDefault(x => x.Id == userId);
+            var completedSurveys=user.CompletedSurveys;
+
+
+            if (completedSurveys.Count > 0 && completedSurveys!=null)
+            {
+                XLWorkbook workbook = new XLWorkbook();
+                foreach (var survey in completedSurveys)
+                {
+                    var sheetName = 
+                        survey.DateOfComplete.Day.ToString()+"_"
+                        + survey.DateOfComplete.Month.ToString()+"_"
+                        + survey.DateOfComplete.Year.ToString() + "_"
+                        + survey.DateOfComplete.Hour.ToString() + "_"
+                        + survey.DateOfComplete.Minute.ToString() + "_"
+                        + survey.DateOfComplete.Second.ToString();
+                    IXLWorksheet worksheet = workbook.Worksheets.Add(sheetName);
+                    int n= 1;
+                    worksheet.Cell(n, 1).Value = "Ankieta:";
+                    worksheet.Cell(n, 2).Value = survey.Survey.Id;
+                    n++;
+                    worksheet.Cell(n, 1).Value = "Autor:";
+                    worksheet.Cell(n, 2).Value = survey.Survey.Author.UserName;
+                    n++;
+                    n++;
+                    worksheet.Cell(n,1).Value = "Pytanie:";
+                    worksheet.Cell(n,2).Value = "Ocena:";
+
+                    foreach (var answer in survey.Answers)
+                    {
+                        worksheet.Cell(n,1).Value = answer.Question.Content;
+                        worksheet.Cell(n,2).Value = answer.AnswerRate;
+                        n++;
+                    }
+
+                }
+
+                Response.ClearContent();
+                Response.Clear();
+                Response.Buffer = true;
+                Response.Charset = "";
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", string.Format("attachment; filename={0}.xls",user.UserName));
+                using (MemoryStream MyMemoryStream = new MemoryStream())
+                {
+                    workbook.SaveAs(MyMemoryStream);
+                    MyMemoryStream.WriteTo(Response.OutputStream);
+                    Response.Flush();
+                    Response.End();
+                }
+
+            }
+            else
+            {
+                ViewBag.Message = string.Format("Nie można pobrać wyników ankiet tego pacjenta, ponieważ nie uzupełnił on jeszcze żadnej ankiety");
+            }
+
+            return View("ShowStudents", students);
         }
     }
 }
