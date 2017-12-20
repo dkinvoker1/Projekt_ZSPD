0fa3b6ce8a0ff7b1afe5b19bea77257bd3ee226b
Marta <marta.marszalekk@gmail.com>
2017-12-20 09:22:41 +0100

    Add tests project

---
+++ ZSPD_Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZSPD_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //to be continued
+        }
+    }
+}
