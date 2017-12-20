9923666ae7486e61f88d078d9c74ec545718dbc5
wojbod93 <wojciech.bodusz93@gmail.com>
2017-12-20 13:01:47 +0100

    Dodanie widoków na potrzeby AZ - podawanie swojego poziomu wiedzy oraz wybór poziomu zadania.

---
+++ ZSPD/Controllers/Student/ExerciseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZSPD.Controllers.Student
+{
+    public class ExerciseController : Controller
+    {
+        // GET: Exercise
+        public ActionResult ChoseYourLevel()
+        {
+            return View();
+        }
+
+        public ActionResult ChoseExerciseLevel()
+        {
+            return View();
+        }
+    }
+}
