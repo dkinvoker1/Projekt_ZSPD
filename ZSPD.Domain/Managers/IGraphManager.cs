830d1e31032e17754046f1e99e1ba69e6518398a
daniell132 <daniell132@wp.pl>
2018-01-17 00:18:25 +0100

    Dodawanie nowego grafu z plików do bazy danych

---
+++ ZSPD.Domain/Managers/IGraphManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ZSPD.Domain.Models.EntityModels;
+
+namespace ZSPD.Domain.Managers
+{
+    public interface IGraphManager
+    {
+        void AddGraphFromFiles(string subjectName, string path1, string path2, string path3);
+        List<SubjectGraph> GetAllGraphs();
+    }
+}
---
+++ ZSPD/Controllers/AdminController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZSPD.Domain.Managers;
+using ZSPD.Domain.Models.EntityModels.Accounts;
+using ZSPD.Models;
+
+namespace ZSPD.Controllers
+{
+    [Authorize(Roles = Roles.Admin)]
+    public class AdminController : Controller
+    {
+        IGraphManager _graphManager;
+        IStudentManager _studentManager;
+        IPsychologistManager _psychologistManager;
+        public AdminController(IGraphManager graphManager, 
+                                IStudentManager studentManager, 
+                                IPsychologistManager psychologistManager)
+        {
+            _graphManager = graphManager;
+            _studentManager = studentManager;
+            _psychologistManager = psychologistManager;
+        }
+        // GET: Admin
+        public ActionResult AddGraph()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadGraph(string name, IEnumerable<HttpPostedFileBase> files)
+        { 
+            List<string> paths = new List<string>();
+            foreach (var file in files)
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/Uploads"), fileName);
+                   
+                    file.SaveAs(path);
+
+                    paths.Add(path);
+                }
+            }
+
+            string poj = System.IO.File.ReadAllText(paths[0]); // Cofnięcie o 1 folder w drzewku
+            // wczytanie z pliku tekstowego wartosci dla poprzednikow
+            string poj1 = System.IO.File.ReadAllText(paths[1]);
+            // wczytanie z pliku tekstowego zadan dla odp pojec
+            string zad = System.IO.File.ReadAllText(paths[2]);
+
+            _graphManager.AddGraphFromFiles(name, paths[0], paths[1], paths[2]);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult StudentsManage()
+        {
+            var allStudents = _psychologistManager.GetAllStudents();
+            return View(allStudents);
+        }
+
+        public ActionResult StudentDetails(string id)
+        {
+            var actualStudent = _psychologistManager.GetAllStudents().FirstOrDefault(x => x.Id == id);
+            var allSubjects = _graphManager.GetAllGraphs();
+            var VM = new StudentDetailsViewModel(actualStudent, allSubjects);  
+
+            return View(VM);
+        }
+
+        [HttpPost]
+        public ActionResult AddNewSubject(StudentDetailsViewModel vm)
+        {
+            var allSubject = _graphManager.GetAllGraphs();
+
+            _studentManager.SetActiveSubject(vm.ActualStudent.Id, allSubject.First(x => x.Name == vm.SelectedSubject).Id);
+            return RedirectToAction("StudentsManage");
+        }
+    }
+}
